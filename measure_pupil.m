6adfb76965a0b9874b3beb7731324db1310da29b
Aeron Laffere <20013881+aeronlaffere@users.noreply.github.com>
2022-11-25 16:30:18 +0000

    Tidy measure_pupil function

--- measure_pupil.m
+++ measure_pupil.m
@@ -1,73 +1,64 @@
-addpath utility\
+function measure_pupil(exp_ref)
 
-original_video = VideoReader('2021-05-13_2_DAP029_eye.mp4');
-num_frames = original_video.NumFrames;
-
-test_frame = read(original_video, 10);
-test_frame = min(test_frame, [], 3);
-
-initial_low_threshold = 0.0;
-initial_high_threshold = 48;
-
-[low_threshold, high_threshold] = ...
-    threshold(initial_low_threshold, initial_high_threshold, test_frame);
-
-source_video_array = read(original_video, [1 num_frames]);
-video_array = min(source_video_array, [], 3);
-video_array = (video_array >= low_threshold) & (video_array <= high_threshold);
-
-
-radius_list = nan(num_frames, 1);
-center_x_list = nan(num_frames, 1);
-center_y_list = nan(num_frames, 1);
-
-ppm = ParforProgressbar(num_frames);
-
-parfor i = 1 : num_frames
-    tic
-    this_frame = video_array(:, :, :, i);
-    this_frame = bwareafilt(this_frame, 1);
-    this_frame = imfill(this_frame, 'holes');
+    original_video = VideoReader([exp_ref '_eye.mp4']);
+    num_frames = original_video.NumFrames;
     
-    [center, radius, metric] = imfindcircles(this_frame, [8, 30]);
-    if numel(radius) == 1
-        radius_list(i) = radius;
-    else
-        radius_list(i) = NaN;
-    end
-
-    if numel(center) == 2
-        center_x_list(i) = center(1);
-        center_y_list(i) = center(2);
-    else
-        center_x_list(i) = NaN;
-        center_y_list(i) = NaN;
+    % Set the threshold
+    test_frame = min(read(original_video, 10), [], 3);
+    initial_low_threshold = 0.0;
+    initial_high_threshold = 48;
+    
+    [low_threshold, high_threshold] = ...
+        threshold(initial_low_threshold, initial_high_threshold, test_frame);
+    %TODO: Make threshold checking more intelligent, allowing setting ahead
+    %of time and checking at this stage if previous threshold exists. Use
+    %flag in function to force overwrite threshold if previous exists.
+    %e.g., (exp_ref, 'force', true)
+    
+    % Process video
+    source_video_array = read(original_video, [1 num_frames]);
+    video_array = min(source_video_array, [], 3);
+    video_array = (video_array >= low_threshold) & (video_array <= high_threshold);
+    
+    radius_list = nan(num_frames, 1);
+    center_x_list = nan(num_frames, 1);
+    center_y_list = nan(num_frames, 1);
+    
+    ppm = ParforProgressbar(num_frames);
+    
+    parfor i = 1 : num_frames
+    
+        this_frame = video_array(:, :, :, i);
+        this_frame = bwareafilt(this_frame, 1);
+        this_frame = imfill(this_frame, 'holes');
+        
+        [center, radius, ~] = imfindcircles(this_frame, [8, 30]);
+        if numel(radius) == 1
+            radius_list(i) = radius;
+        else
+            radius_list(i) = NaN;
+        end
+    
+        if numel(center) == 2
+            center_x_list(i) = center(1);
+            center_y_list(i) = center(2);
+        else
+            center_x_list(i) = NaN;
+            center_y_list(i) = NaN;
+        end
+        
+        ppm.increment();
+    
     end
     
-    ppm.increment();
-    toc
-end
+    delete(ppm);
+    
+    frames = 1:num_frames;
+    t_eye = getEventTimes(exp_ref, 'eye_camera_strobe'); 
 
-delete(ppm);
-figure;
-subplot(1, 2, 1)
-plot(radius_list)
+    output_table = table(frames', t_eye, radius_list, center_x_list, center_y_list);
+    output_table.Properties.VariableNames = {'frame', 'timeline', 'radius', 'center_x', 'center_y'};
 
-frames = 1:num_frames;
-t_eye = getEventTimes('2021-05-13_2_DAP029','eye_camera_strobe'); 
-output_table = table(frames', t_eye, radius_list, center_x_list, center_y_list);
-output_table.Properties.VariableNames = {'frame', 'timeline', 'radius', 'center_x', 'center_y'};
-writetable(output_table, '2021-05-13_2_DAP029_eye.csv')
+    writetable(output_table, [exp_ref '_eye_.csv'])
 
-
-center_x_list = output_table.center_x;
-center_y_list = output_table.center_y;
-radius_list = output_table.radius;
-subplot(1, 2, 2)
-for i = 1 : num_frames
-    disp(i)
-    this_frame = source_video_array(:, :, :, i);
-    imshow(this_frame);
-    viscircles([center_x_list(i) center_y_list(i)], radius_list(i), 'LineWidth', 0.5);
-    annotated_frame = getframe;
 end

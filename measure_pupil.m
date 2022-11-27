d1361076efd5ee1ea43353fb9a4fbced1ffbba64
Aeron Laffere <20013881+aeronlaffere@users.noreply.github.com>
2022-11-27 18:22:27 +0000

    Update measure_pupil.m

--- measure_pupil.m
+++ measure_pupil.m
@@ -4,7 +4,7 @@ function measure_pupil(exp_ref)
     num_frames = original_video.NumFrames;
     
     % Set the threshold
-    test_frame = min(read(original_video, 10), [], 3);
+    test_frame = min(read(original_video, randi(num_frames, 1, 1)), [], 3);
     initial_low_threshold = 0.0;
     initial_high_threshold = 48;
     
@@ -14,7 +14,7 @@ function measure_pupil(exp_ref)
     %of time and checking at this stage if previous threshold exists. Use
     %flag in function to force overwrite threshold if previous exists.
     %e.g., (exp_ref, 'force', true)
-    
+
     % Process video
     source_video_array = read(original_video, [1 num_frames]);
     video_array = min(source_video_array, [], 3);
@@ -32,7 +32,7 @@ function measure_pupil(exp_ref)
         this_frame = bwareafilt(this_frame, 1);
         this_frame = imfill(this_frame, 'holes');
         
-        [center, radius, ~] = imfindcircles(this_frame, [8, 30]);
+        [center, radius, ~] = imfindcircles(this_frame, [8, 30], 'ObjectPolarity', 'Dark', 'EdgeThreshold', 0.6);
         if numel(radius) == 1
             radius_list(i) = radius;
         else
@@ -59,6 +59,6 @@ function measure_pupil(exp_ref)
     output_table = table(frames', t_eye, radius_list, center_x_list, center_y_list);
     output_table.Properties.VariableNames = {'frame', 'timeline', 'radius', 'center_x', 'center_y'};
 
-    writetable(output_table, [exp_ref '_eye_.csv'])
+    writetable(output_table, [exp_ref '_eye.csv'])
 
 end

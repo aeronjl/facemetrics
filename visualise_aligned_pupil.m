554cb787c04ebb9f5124e23f09e4a434218b6910
Aeron Laffere <20013881+aeronlaffere@users.noreply.github.com>
2022-11-27 18:22:40 +0000

    Update visualise_aligned_pupil.m

--- visualise_aligned_pupil.m
+++ visualise_aligned_pupil.m
@@ -1,15 +1,24 @@
-figure;
-subplot(1, 2, 1)
-plot(radius_list)
+function visualise_aligned_pupil(exp_ref)
+    
+    pupil_data = readtable([exp_ref '_eye.csv'], 'Delimiter', ',');
+    
+    disp('Loading video...')
+    original_video = VideoReader([exp_ref '_eye.mp4']);
+    num_frames = original_video.NumFrames;
+    source_video_array = read(original_video, [1 num_frames]);
+    
+    v = VideoWriter([exp_ref '_eye_annotated.avi']);
+    open(v)
 
-center_x_list = output_table.center_x;
-center_y_list = output_table.center_y;
-radius_list = output_table.radius;
-subplot(1, 2, 2)
-for i = 1 : num_frames
-    disp(i)
-    this_frame = source_video_array(:, :, :, i);
-    imshow(this_frame);
-    viscircles([center_x_list(i) center_y_list(i)], radius_list(i), 'LineWidth', 0.5);
-    annotated_frame = getframe;
+    disp('Visualising...')
+
+    for i = 1 : num_frames
+        this_frame = source_video_array(:, :, :, i);
+        imshow(this_frame);
+        viscircles([pupil_data.center_x(i) pupil_data.center_y(i)], pupil_data.radius(i), 'LineWidth', 0.5);
+        annotated_frame = getframe(gcf);
+        writeVideo(v, annotated_frame);
+    end
+
+    close(v);
 end
